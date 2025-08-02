ea8bc7a06a7d2f9dd830f1988a874649436ad53a
atshaw1994 <atshaw1994@gmail.com>
2025-08-02 12:37:25 -0400

    Reset result_txt when resetting the buttons

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -84,6 +84,7 @@ function playGame(humanChoice) {
         humanScore = 0
         computerScore = 0
         setBtnsText("Rock", "Paper", "Scissors")
+        result_txt.innerHTML = "Press a button to play"
     }
 }
 
